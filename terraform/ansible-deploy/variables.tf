df9c90173b76fb08a0f5779ac65f9fe7ae66aad3
AbhishekPattnaik04 <58695787+AbhishekPattnaik04@users.noreply.github.com>
2020-02-09 10:07:03 +0530

    Update variables.tf

--- terraform/ansible-deploy/variables.tf
+++ terraform/ansible-deploy/variables.tf
@@ -1,11 +1,11 @@
 variable "key_name" {
-  default = "music"
+  default = "capstone301"
 }
 
 variable "pvt_key" {
-  default = "/home/mtadminnuvepro/.ssh/music.pem"
+  default = "/home/mtadminnuvepro/.ssh/capstone301.pem"
 }
 
 variable "sg-id" {
-  default = "sg-0698ac11f618df3a5"
+  default = "sg-0aa372d197dc34c80"
 }
